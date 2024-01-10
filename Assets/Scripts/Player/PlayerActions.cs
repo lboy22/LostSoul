cfbacd3374f7e92c3529426776c10d40e11e12e0
fight3rmn <benballard2000@gmail.com>
2024-01-10 11:47:56 -0800

    "added the ability to move up vertical bridges"

---
+++ Assets/Scripts/Player/PlayerActions.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerActions : MonoBehaviour
+{
+    //Use this to check whether the player can do any actions (moving, jumping or creating bridges).
+    // This is so the player doesn't do any of these while the game is in pause state.
+    // [SerializeField] static bool isPause = false;
+    [SerializeField] int moveSpeed = 5;
+    [SerializeField] Rigidbody2D playerBody;
+    [SerializeField] PlayerControls playerControls;
+    public GameObject playerObject;
+    int yValue;
+
+    private InputAction move;
+    Vector2 moveDirection = Vector2.zero;
+    [SerializeField] int jumpForce = 3;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        move = playerControls.Player.Move;
+        move.Enable();
+    }
+
+    private void OnDisable()
+    {
+        move.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = move.ReadValue<Vector2>();
+        if(Input.GetKeyDown("w")) {
+            yValue = 1;
+        }
+        else if(Input.GetKeyDown("s")) {
+            yValue = -1;
+        }
+        if(Input.GetKeyUp("w")) {
+            yValue = 0;
+        }
+        if(Input.GetKeyUp("s")) {
+            yValue = 0;
+        }
+        
+        //if (playerControls.Player.Jump.triggered) 
+        //{
+        //    OnJump();
+       // }
+        
+    }
+
+    //private void OnJump()
+    //{
+     //   Vector2 currentVelocity = playerBody.velocity;
+       // currentVelocity.y = jumpForce;
+    //    playerBody.velocity = currentVelocity;
+
+      //  Debug.Log("Jumping: " + jumpForce + " Velocity: " + playerBody.velocity);
+   // }
+
+
+    private void FixedUpdate()
+    {
+        
+        if(playerObject.transform.position.x > PlayerPrefs.GetInt("bridgeLeft") && playerObject.transform.position.x < PlayerPrefs.GetInt("bridgeRight") && playerObject.transform.position.y >= PlayerPrefs.GetInt("bridgeBottom") && playerObject.transform.position.y <= PlayerPrefs.GetInt("bridgeTop")) {
+            playerBody.velocity = new Vector2(moveDirection.x * moveSpeed, yValue * moveSpeed);
+        }
+        else {
+            playerBody.velocity = new Vector2(moveDirection.x * moveSpeed, 0);
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/bridgeBuild.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bridgeBuild : MonoBehaviour
+{
+    public float xCoord;
+    public float yCoord;
+    Vector3 bottomLeftCoords;
+    public GameObject player;
+    Vector3 topRightCoords;
+    float xx;
+    public GameObject thisObject;
+    void Start() 
+    {
+        PlayerPrefs.SetInt("bridgeLeft", 0);
+            PlayerPrefs.SetInt("bridgeBottom", 0);
+            PlayerPrefs.SetInt("bridgeRight", 0);
+            PlayerPrefs.SetInt("bridgeTop", 0);
+        bottomLeftCoords = new Vector3(xCoord, yCoord, 0) - new Vector3(5.0f, 7.0f, 25.0f);
+        topRightCoords = new Vector3(xCoord, yCoord, 0) + new Vector3(5.0f, 7.0f, 25.0f);
+        print(bottomLeftCoords);
+        print(topRightCoords);
+    }
+
+
+    bool compareVector3(Vector3 v1, Vector3 v2) 
+    {
+        if(v1.x > v2.x && v1.y>v2.y) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        xx = Camera.main.ScreenToWorldPoint(new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 1.0f)).x;
+        //print("mouse position is " + xx);
+        //if(xx < -5.0f && Input.GetMouseButtonDown(0)) {
+        //    thisObject.transform.position = new Vector3(-23.0f, 6.0f, 1.0f);
+        //}
+        //&& compareVector3(Camera.main.ScreenToWorldPoint(Input.mousePosition), bottomLeftCoords) && compareVector3(Camera.main.ScreenToWorldPoint(topRightCoords), Input.mousePosition)
+        if(Input.GetMouseButtonDown(0) ) {
+            thisObject.transform.position = new Vector3(-20.0f, 1.5f, 1.0f);
+            print ("updated");
+            PlayerPrefs.SetInt("bridgeLeft", -21);
+            PlayerPrefs.SetInt("bridgeRight", -18);
+            PlayerPrefs.SetInt("bridgeTop", 7);
+        }
+    }
+}
